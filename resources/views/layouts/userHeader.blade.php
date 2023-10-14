ea73502b67eb638352d0a13606e73fcb80664a2f
Gerges Medhat <gergesmedhat414@gmail.com>
2023-10-14 11:41:05 +0100

    navbars

--- resources/views/layouts/userHeader.blade.php
+++ resources/views/layouts/userHeader.blade.php
@@ -20,9 +20,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+             
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -30,13 +28,27 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                    <a class="navbar-brand" href="{{('home') }}">{{ __('Home') }}</a>
 
+
+                <a href="{{route('products.index')}}" class="navbar-brand">My Orders</a>
+             
+                
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                          
+
+                              
                         <!-- Authentication Links -->
+                        
+                  
+        
+                           
+                        
                         @guest
+                  
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -48,6 +60,7 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+                           
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -75,6 +88,11 @@
         <main class="py-4">
             @yield('content')
         </main>
+        
+        <div class="bg-danger text-white">
+              @yield('body')
+
+        </div>
     </div>
 </body>
 </html>
