596838daf50aca12df8601915fa3379f3f875f23
Gerges Medhat <gergesmedhat414@gmail.com>
2023-10-15 23:30:39 +0100

    headers

--- resources/views/layouts/adminHeader.blade.php
+++ resources/views/layouts/adminHeader.blade.php
@@ -60,6 +60,12 @@
                                 </li>
                             @endif
 
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+
                           
                            
                         @else
