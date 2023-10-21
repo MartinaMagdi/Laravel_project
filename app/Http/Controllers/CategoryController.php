c1c9e38d19c475d58140afa6b1821a6e417d35a7
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-21 04:55:17 +0300

    Prevent users from accessing categories routes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,7 +20,7 @@ class CategoryController extends Controller
     function __construct(){
 
         // $this->middleware('is-admin')->only(['create','edit','destroy']);       // only admin  can add or edit or delete category
-        $this->middleware('is_admin')->only(['create', 'edit','destroy']);
+        $this->middleware('is_admin');
   }
 
     /**
